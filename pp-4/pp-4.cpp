6186d457c9670acb87f0cf4d74ec06be0ff0634f
gleb <deffender1337@gmail.com>
2025-04-29 20:37:49 +0300

    Reduce default matrix size in pp-4.cpp
    Changed the initialization of variable `n` from 1024 to 128 in `pp-4.cpp`. The code still allows for overriding this value via command-line arguments, likely to facilitate testing or improve performance.

--- pp-4/pp-4.cpp
+++ pp-4/pp-4.cpp
@@ -168,7 +168,7 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int n = 1024;
+    int n = 128;
     if (argc >= 2) n = atoi(argv[1]);
 
     Matrix A, B, C_std, C_seq, C_par;
